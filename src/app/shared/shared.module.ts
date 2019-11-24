a4f165af08ef7432760a4e0901d323d5c1ec4867
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-11-24 07:59:43 -0500

    adding flex-layout, angular material and hammerjs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MaterialModule} from './components/material.module';
 
+const COMPONENTS = [
 
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ...COMPONENTS,
+    ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    ...COMPONENTS
   ]
 })
 export class SharedModule { }
