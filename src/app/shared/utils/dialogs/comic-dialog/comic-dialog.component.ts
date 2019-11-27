8fbf94572555b57b6c91fad64044ba6094735284
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-11-26 21:56:33 -0500

    changes in modal design

--- src/app/shared/utils/dialogs/comic-dialog/comic-dialog.component.ts
+++ src/app/shared/utils/dialogs/comic-dialog/comic-dialog.component.ts
@@ -1,13 +1,19 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-comic-dialog',
   templateUrl: './comic-dialog.component.html',
   styleUrls: ['./comic-dialog.component.scss']
 })
-export class ComicDialogComponent {
+export class ComicDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {
+  }
 
+  ngOnInit(): void {
+
+  }
 }
