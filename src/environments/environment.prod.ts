4956e78eab9339bdfafe8270e8f93dc3cabfab4a
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-11-27 18:05:53 -0500

    add credentials to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  backend: {
+    host: 'https://gateway.marvel.com:443',
+    private_key: '716c70869dcd5289b59db2d504d3ae68f8cae10a',
+    public_key: '5dc305d7a3a4daf528658071d7b4248d',
+  }
 };
